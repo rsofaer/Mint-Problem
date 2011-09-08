a048c441419753f1fc95ca319c7f8c1f237b06de
Raphael Sofaer <raphael@joindiaspora.com>
2011-09-08 14:37:07 -0400

    update web file

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
     [[org.clojure/clojure "1.2.1"]
      [ring/ring-jetty-adapter "0.3.9"]
      [org.clojure/clojure-contrib "1.2.0"]
-     [compojure "0.6.4"]
+     [compojure "0.6.5"]
      [org.danlarkin/clojure-json "1.2-SNAPSHOT"]]
   :dev-dependencies [[lein-ring "0.4.5"]]
   :ring {:handler mint.web/app})
