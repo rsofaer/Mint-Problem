949e7667e2388af42f3f7441c47ec7bb86bf3a17
Raphael Sofaer <raphael@joindiaspora.com>
2011-09-08 14:41:15 -0400

    comment out unused routes for now

--- src/mint/web.clj
+++ src/mint/web.clj
@@ -4,12 +4,10 @@
   (:require [compojure.route :as route]
             [compojure.handler :as handler]))
 
-(defn index-page []
-  "<h1> Hello from Raphael!</h1>")
 (defroutes main-routes
-  (GET "/" [] (index-page))
-  (route/resources "/") ;maps to the public/ directory
-  (route/not-found "Page not found"))
+  (GET "/" [] "<h1> Hello from Raphael!</h1>"))
+  ;(route/resources "/") ;maps to the public/ directory
+  ;(route/not-found "Page not found"))
 
 (defn app [req]
    (handler/site main-routes))
