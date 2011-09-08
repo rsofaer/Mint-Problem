5757ee95df67d8cacf5a4c55cd49684fad8fee9d
Raphael Sofaer <raphael@joindiaspora.com>
2011-09-08 13:44:26 -0400

    Trying to use compojure

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
     [[org.clojure/clojure "1.2.1"]
      [ring/ring-jetty-adapter "0.3.9"]
      [org.clojure/clojure-contrib "1.2.0"]
+     [compojure "0.6.4"]
      [org.danlarkin/clojure-json "1.2-SNAPSHOT"]])
 
--- src/mint/web.clj
+++ src/mint/web.clj
@@ -1,10 +1,15 @@
 (ns mint.web
-  (:use ring.adapter.jetty))
+  (:use compojure.core)
+  (:require [compojure.route :as route]
+            [compojure.handler :as handler]))
+
+(defroutes main-routes
+  (GET "/" [] "<h1>Hello from Raphael!</h1>")
+  (route/resources "/") ;maps to the public/ directory
+  (route/not-found "Page not found"))
 
 (defn app [req]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Raphael says: Hello from Clojure!\n"})
+   (handler/site main-routes))
 
 (defn -main []
   (let [port (Integer/parseInt (System/getenv "PORT"))]
